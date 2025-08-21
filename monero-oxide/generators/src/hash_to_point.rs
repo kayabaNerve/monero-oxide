d59c5f493a57c39277ee1f3c894447af45cff372
Luke Parker <lukeparker5132@gmail.com>
2025-08-21 00:33:08 -0400

    Successfully identity step_2, step_3

--- monero-oxide/generators/src/hash_to_point.rs
+++ monero-oxide/generators/src/hash_to_point.rs
@@ -28,13 +28,14 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
     Curve25519 is a Montgomery curve with equation v^2 = u^3 + 486662 u^2 + u.
 
     A Curve25519 point `(u, v)` may be mapped to an Ed25519 point `(x, y)` with the map
-    `(sqrt(-(A+2)) u/v, (u-1) / (v+1))`.
+    `(sqrt(-(A + 2)) u / v, (u - 1) / (u + 1))`.
   */
   #[allow(non_snake_case)]
   let A = FieldElement::from(486662u64);
   #[allow(non_snake_case)]
   let negative_A = -A;
 
+  // OPEN QUESTION: What is this `step_1` value?
   let step_1 = {
     use crypto_bigint::{Encoding, U256};
     FieldElement::from_u256(&U256::from_le_bytes(keccak256(&bytes)))
@@ -42,13 +43,28 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
   let step_1 = step_1.square();
   let step_1 = step_1.double();
 
-  let step_2 = step_1 + FieldElement::ONE;
+  /*
+    `z` is used as the denominator within projective coordinates for a point on Curve25519. We know
+    it's non-zero as:
 
-  // derivative_was_square = (step_2 / (step_2^2 - (A^2 * step_1))).is_square()
-  let derivative_was_square = {
-    // The inputs to the following inlined function, in its preferred notation
-    let u = step_2;
-    let v = step_2.square() - (A.square() * step_1);
+    ```sage
+    p = 2**255 - 19
+    Mod((p - 1) * inverse_mod(2, p), p).is_square() == False
+    ```
+  */
+  let z = step_1 + FieldElement::ONE;
+
+  // u_over_v = z / (z^2 - (A^2 * step_1))
+  // u_over_v_was_square = u_over_v.is_square()
+  let u_over_v_was_square = {
+    /*
+      The following inlined function defines its arguments as `u, v`, which conflicts with how
+      Curve25519 points are considered `(u, v)`. These are NOT coordinates for a point on
+      Curve25519.
+    */
+    // OPEN QUESTION: Why are these the values used here?
+    let u = z;
+    let v = z.square() - (A.square() * step_1);
 
     /*
       sqrt_ratio_i(u, v) primarily calculates the square root of `u / v`, at roughly half the cost
@@ -59,7 +75,7 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
       The following implements parts of ristretto.group's detailing of the algorithm, in its
       notation.
     */
-    let derivative_was_square = {
+    let u_over_v_was_square = {
       // Step 1
       #[allow(non_snake_case)]
       let r = {
@@ -89,11 +105,12 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
     };
 
     debug_assert_eq!(
-      bool::from(derivative_was_square),
-      Option::<FieldElement>::from((u * v.invert().unwrap()).sqrt()).is_some()
+      bool::from(u_over_v_was_square),
+      Option::<FieldElement>::from((u * v.invert().unwrap()).sqrt()).is_some(),
+      "sqrt_ratio_i implementation returned an incorrect result"
     );
 
-    derivative_was_square
+    u_over_v_was_square
   };
 
   /*
@@ -105,37 +122,40 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
     from its coordinates.
   */
 
-  let step_3 =
-    FieldElement::conditional_select(&(negative_A * step_1), &negative_A, !derivative_was_square);
+  /*
+    This is the _Projective_ `u` coordinate for a point on Curve25519. The Affine `u` coordinate
+    would be `u / z`.
+  */
+  // OPEN QUESTION: Why is this a coordinate of a point on Curve25519? Is it uniform?
+  let u =
+    FieldElement::conditional_select(&(negative_A * step_1), &negative_A, !u_over_v_was_square);
 
   /*
-    sign = !derivative_was_square
+    Map from the Curve25519 `u` coordinate to an Ed25519 `y` coordinate.
 
-    If sign, `step_3 = -486662`, else, `step_3 = -486662 * step_1`
-    `step_2 = step_1 + 1`
+    Instead of normalizing, then mapping, the following equation optimizes to a single inversion.
 
-    We need `step_3 + step_2 \ne 0`, which would require `step_3 \cong -step_2 \mod 2^{255}-19`.
-    This requires:
-    - If `sign`, `step_1 \mod 2^{255}-19 \ne 486661`.
-    - If `!sign`, `(step_1 + 1) \mod 2^{255}-19 \ne (step_1 * 486662) \mod 2^{255}-19` which is
-      equivalent to `(step_1 * 486661) \mod 2^{255}-19 \ne 1`.
-
-    In summary, if `sign`, `step_1` must not equal `486661`, and if `!sign`, `step_1` must not be
-    the multiplicative inverse of `486661`. Since `step_1` is the output of a hash function, this
-    should have negligible probability. Additionally, since the definition of `sign` is dependent
-    on `step_1`, it may be truly impossible to reach.
+    If `u / z` is actually the coordinate of a point on Curve25519, the following `unwrap` calls
+    will never trigger due to the map from Curve25519 to Ed25519 being well-defined.
   */
-  #[allow(non_snake_case)]
-  let Y = (step_3 - step_2) *
-    (step_3 + step_2).invert().expect(&format!(
-      "step_1 ({:?}) was 486661 or 486661^{{-1}} depending on was_square ({}). input: {:?}",
-      step_1,
-      bool::from(derivative_was_square),
-      bytes
-    ));
+  let y = (u - z) * (u + z).invert().unwrap();
+  debug_assert_eq!(
+    y,
+    {
+      let u = u * z.invert().expect("unreachable modulo 2^{255} - 19");
+      (u - FieldElement::ONE) * (u + FieldElement::ONE).invert().unwrap()
+    },
+    "normalize and map wasn't equivalent to normalize, then map"
+  );
 
-  let mut bytes = Y.to_repr();
-  bytes[31] |= (!derivative_was_square).unwrap_u8() << 7;
+  // Encode the `y` coordinate for us to then pass to curve25519_dalek
+  let mut bytes = y.to_repr();
+  // Add the sign bit for which `x` coordinate to take.
+  /*
+    OPEN QUESTION: How does whether this value was square translate to which `x` coordinate to
+    take?
+  */
+  bytes[31] |= (!u_over_v_was_square).unwrap_u8() << 7;
 
   /*
     Ed25519 point decompression works as follows.
@@ -151,7 +171,9 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
     d = (-121665) * inverse_mod(121666, p)
     Mod((p - 1) * inverse_mod(d, p), p).is_square() == False
     ```
-  */
 
+    If `u / z` is actually the coordinate of a point on Curve25519, `y` will actually be the
+    coordinate of a point on Ed25519 due to the map from Curve25519 to Ed25519 being well-defined.
+  */
   decompress_point(bytes).expect("point from hash-to-curve wasn't on-curve").mul_by_cofactor()
 }
