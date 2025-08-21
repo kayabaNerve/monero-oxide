a8dd06d261df7cdf0ad1b8521025e8bacfc49e8f
Luke Parker <lukeparker5132@gmail.com>
2025-08-21 01:24:47 -0400

    Move inlined sqrt_ratio_i to its own functions
    Also document why the initially sampled FieldElement presumably isn't biased.

--- monero-oxide/generators/src/hash_to_point.rs
+++ monero-oxide/generators/src/hash_to_point.rs
@@ -86,6 +86,52 @@ fn curve25519_u_z_and_x_sign_to_ed25519_point_slow(
   .unwrap()
 }
 
+/*
+  `sqrt_ratio_i(u, v)` (where `u, v` represent any elements of the field) is a frequently-defined
+  function to calculate the square root of `u / v`. The benefit of such an explicit function is
+  it is _much faster_ than explicitly calculating `sqrt(u * v.invert())`.
+
+  `sqrt_ratio_i(u, v)` traditionally returns if `u / v` is square and the square root if so. For
+  documentation, please see the following links:
+  - https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.3
+  - https://ristretto.group/formulas/invsqrt.html
+
+  `is_square_ratio_i(u, v)` solely returns if `u / v` is square and doesn't finish the square-root
+  calculation. The internal methodology is identical however, following the notation from
+  ristretto.group.
+*/
+fn is_square_ratio_i(u: FieldElement, v: FieldElement) -> Choice {
+  // Step 1
+  #[allow(non_snake_case)]
+  let r = {
+    let v3 = v * v * v;
+    let uv3 = u * v3;
+    let v7 = v3 * v3 * v;
+    let uv7 = u * v7;
+    uv3 *
+      uv7.pow(
+        (-FieldElement::from(5u8)) *
+          FieldElement::from(8u8).invert().expect("eight was coprime with the prime 2^{255}-19"),
+      )
+  };
+  // Step 2
+  let c = r.square() * v;
+
+  // Step 3
+  let correct_sign_sqrt = c.ct_eq(&u);
+  // Step 4
+  let flipped_sign_sqrt = c.ct_eq(&-u);
+
+  // Skips steps 5-7, not updating (nor returning) `r`
+
+  correct_sign_sqrt | flipped_sign_sqrt
+}
+
+#[cfg(debug_assertions)]
+fn is_square_ratio_i_slow(u: FieldElement, v: FieldElement) -> Choice {
+  (u * v.invert().unwrap()).sqrt().is_some()
+}
+
 /// Monero's `hash_to_ec` function.
 ///
 /// This achieves parity with https://github.com/monero-project/monero
@@ -116,6 +162,17 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
   // OPEN QUESTION: What is this `step_1` value?
   let step_1 = {
     use crypto_bigint::{Encoding, U256};
+    /*
+      This isn't a wide reduction, implying it'd be biased, yet the bias should only be negligible
+      due to the shape of the prime number. All elements within the prime field field have a
+      `2 / 2^{256}` chance of being selected, except for the first 19 which have a `3 / 2^256`
+      chance of being selected. In order for this 'third chance' (the bias) to be relevant, the
+      hash function would have to output a number greater than or equal to:
+
+        0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffda
+
+      which is of negligible probability.
+    */
     FieldElement::from_u256(&U256::from_le_bytes(keccak256(&bytes)))
   };
   let step_1 = step_1.square().double();
@@ -133,63 +190,24 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
   */
   let z = step_1 + FieldElement::ONE;
 
-  // u_over_v = z / (z^2 - (A^2 * step_1))
-  // u_over_v_was_square = u_over_v.is_square()
-  let u_over_v_was_square = {
-    /*
-      The following inlined function defines its arguments as `u, v`, which conflicts with how
-      Curve25519 points are considered `(u, v)`. These are NOT coordinates for a point on
-      Curve25519.
-    */
+  let was_square = {
     // OPEN QUESTION: Why are these the values used here?
-    let u = z;
-    let v = z.square() - (A.square() * step_1);
-
+    let num = z;
     /*
-      sqrt_ratio_i(u, v) primarily calculates the square root of `u / v`, at roughly half the cost
-      of calculating `sqrt(u * v.invert())`. Documentation be found at the following links:
-      - https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.3
-      - https://ristretto.group/formulas/invsqrt.html
+      OPEN QUESTION: Why will `denom` be non-zero? Statistically negligible probability?
 
-      The following implements parts of ristretto.group's detailing of the algorithm, in its
-      notation.
+      We don't actually need `denom` to be non-zero. `is_square_ratio_i` won't panic even if
+      `denom` is zero, yet it won't have well-defined behavior either (due to returning if an
+      undefined number is considered square) which likely complicates the following analysis.
     */
-    let u_over_v_was_square = {
-      // Step 1
-      #[allow(non_snake_case)]
-      let r = {
-        let v3 = v * v * v;
-        let uv3 = u * v3;
-        let v7 = v3 * v3 * v;
-        let uv7 = u * v7;
-        uv3 *
-          uv7.pow(
-            (-FieldElement::from(5u8)) *
-              FieldElement::from(8u8)
-                .invert()
-                .expect("eight was coprime with the prime 2^{255}-19"),
-          )
-      };
-      // Step 2
-      let c = r.square() * v;
-
-      // Step 3
-      let correct_sign_sqrt = c.ct_eq(&u);
-      // Step 4
-      let flipped_sign_sqrt = c.ct_eq(&-u);
-
-      // Skips steps 5-7, not updating (nor returning) `r`
-
-      correct_sign_sqrt | flipped_sign_sqrt
-    };
-
+    let denom = z.square() - (A.square() * step_1);
+    let was_square = is_square_ratio_i(num, denom);
     debug_assert_eq!(
-      bool::from(u_over_v_was_square),
-      Option::<FieldElement>::from((u * v.invert().unwrap()).sqrt()).is_some(),
-      "sqrt_ratio_i implementation returned an incorrect result"
+      bool::from(was_square),
+      bool::from(is_square_ratio_i_slow(num, denom)),
+      "is_square_ratio_i implementation returned an incorrect result"
     );
-
-    u_over_v_was_square
+    was_square
   };
 
   /*
@@ -200,16 +218,15 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
     OPEN QUESTION: Why is this a coordinate of a point on Curve25519, except with negligible
     probability? Is it uniform to the set of all `u` coordinates for points on Curve25519?
   */
-  let u =
-    FieldElement::conditional_select(&(negative_A * step_1), &negative_A, !u_over_v_was_square);
+  let u = FieldElement::conditional_select(&(negative_A * step_1), &negative_A, !was_square);
 
   /*
-    OPEN QUESTION: Why is `!u_over_v_was_square` used for the sign?
+    OPEN QUESTION: Why is `!was_square` used for the sign?
 
     This is probably best answered by implementing the rest of the calculations for the `x`
     coordinate in this function, which will probably show the relation.
   */
-  let x_coordinate_sign = !u_over_v_was_square;
+  let x_coordinate_sign = !was_square;
   let res = curve25519_u_z_and_x_sign_to_ed25519_point(u, z, x_coordinate_sign);
   debug_assert_eq!(res, curve25519_u_z_and_x_sign_to_ed25519_point_slow(u, z, x_coordinate_sign));
   res.mul_by_cofactor()
