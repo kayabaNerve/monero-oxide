6c4afaf31eb18bcd6a9ed6c30428b52e1b75b57e
Luke Parker <lukeparker5132@gmail.com>
2025-08-21 00:00:01 -0400

    Further document and proxide context within hash_to_point
    Removes the single-letter variables (which could've conflicted with existing
    notation) for the equally-ambiguous, non-conflicting step_1, step_2, step_3.
    
    Removes reliance on the `FieldElement::from_square` method.
    
    Adds context on Curve25519 and Ed25519 decoding.
    
    The next step will probably be to resolve what step_[1, 2, 3] actually are and
    assign proper names.

--- monero-oxide/generators/src/hash_to_point.rs
+++ monero-oxide/generators/src/hash_to_point.rs
@@ -1,8 +1,7 @@
 use subtle::{ConstantTimeEq, ConditionallySelectable};
 
-use curve25519_dalek::edwards::EdwardsPoint;
-
 use group::ff::{Field, PrimeField};
+use curve25519_dalek::edwards::EdwardsPoint;
 use dalek_ff_group::FieldElement;
 
 use monero_io::decompress_point;
@@ -17,25 +16,50 @@ use crate::keccak256;
 ///   /blob/389e3ba1df4a6df4c8f9d116aa239d4c00f5bc78/src/crypto/crypto-ops.c#L2309). This
 /// implementation runs in constant time.
 ///
-/// According to the original authors, this would implement https://arxiv.org/abs/0706.1448, yet
-/// the referenced methodology doesn't appear present. Notes by Shen Noether also describe the
-/// algorithm (https://web.getmonero.org/resources/research-lab/pubs/ge_fromfe.pdf), yet without
-/// reviewing its security and in a very straight-forward fashion. This function does attempt to
-/// be well documented to explain the algorithm however.
+/// According to the original authors
+/// (https://web.archive.org/web/20201028121818/https://cryptonote.org/whitepaper.pdf), this would
+/// implement https://arxiv.org/abs/0706.1448, yet the cited methodology doesn't appear present.
+/// Notes by Shen Noether also describe the algorithm
+/// (https://web.getmonero.org/resources/research-lab/pubs/ge_fromfe.pdf), yet without reviewing
+/// its security and in a very straight-forward fashion. This function does attempt to be well
+/// documented to explain the algorithm however.
 pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
-  // Curve25519 is a Montgomery curve with equation y^2 = x^3 + 486662 x^2 + x
+  /*
+    Curve25519 is a Montgomery curve with equation v^2 = u^3 + 486662 u^2 + u.
+
+    A Curve25519 point `(u, v)` may be mapped to an Ed25519 point `(x, y)` with the map
+    `(sqrt(-(A+2)) u/v, (u-1) / (v+1))`.
+  */
   #[allow(non_snake_case)]
   let A = FieldElement::from(486662u64);
+  #[allow(non_snake_case)]
+  let negative_A = -A;
 
-  let v = FieldElement::from_square(keccak256(&bytes)).double();
-  let w = v + FieldElement::ONE;
+  let step_1 = {
+    use crypto_bigint::{Encoding, U256};
+    FieldElement::from_u256(&U256::from_le_bytes(keccak256(&bytes)))
+  };
+  let step_1 = step_1.square();
+  let step_1 = step_1.double();
 
-  let w_over_x_was_square = {
-    let u = w;
-    let v = w.square() + (-A.square() * v);
+  let step_2 = step_1 + FieldElement::ONE;
 
-    // Parts of sqrt_ratio_i, as documented here: https://ristretto.group/formulas/invsqrt.html
-    {
+  // derivative_was_square = (step_2 / (step_2^2 - (A^2 * step_1))).is_square()
+  let derivative_was_square = {
+    // The inputs to the following inlined function, in its preferred notation
+    let u = step_2;
+    let v = step_2.square() - (A.square() * step_1);
+
+    /*
+      sqrt_ratio_i(u, v) primarily calculates the square root of `u / v`, at roughly half the cost
+      of calculating `sqrt(u * v.invert())`. Documentation be found at the following links:
+      - https://datatracker.ietf.org/doc/html/rfc8032#section-5.1.3
+      - https://ristretto.group/formulas/invsqrt.html
+
+      The following implements parts of ristretto.group's detailing of the algorithm, in its
+      notation.
+    */
+    let derivative_was_square = {
       // Step 1
       #[allow(non_snake_case)]
       let r = {
@@ -62,7 +86,14 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
       // Skips steps 5-7, not updating (nor returning) `r`
 
       correct_sign_sqrt | flipped_sign_sqrt
-    }
+    };
+
+    debug_assert_eq!(
+      bool::from(derivative_was_square),
+      Option::<FieldElement>::from((u * v.invert().unwrap()).sqrt()).is_some()
+    );
+
+    derivative_was_square
   };
 
   /*
@@ -73,32 +104,54 @@ pub fn hash_to_point(bytes: [u8; 32]) -> EdwardsPoint {
     This resolves the API boundary of how we cannot instantiate a `curve25519_dalek::EdwardsPoint`
     from its coordinates.
   */
-  let sign = !w_over_x_was_square;
 
-  let mut z = -A;
-  z *= FieldElement::conditional_select(&v, &FieldElement::from(1u8), sign);
-  #[allow(non_snake_case)]
-  let Z = z + w;
-  #[allow(non_snake_case)]
-  let mut Y = z - w;
+  let step_3 =
+    FieldElement::conditional_select(&(negative_A * step_1), &negative_A, !derivative_was_square);
 
   /*
-    If sign, `z = -486662`, else, `z = -486662 * v`
-    `w = v + 1`
+    sign = !derivative_was_square
 
-    We need `z + w \ne 0`, which would require `z \cong -w \mod 2^{255}-19`. This requires:
-    - If `sign`, `v \mod 2^{255}-19 \ne 486661`.
-    - If `!sign`, `(v + 1) \mod 2^{255}-19 \ne (v * 486662) \mod 2^{255}-19` which is equivalent to
-      `(v * 486661) \mod 2^{255}-19 \ne 1`.
+    If sign, `step_3 = -486662`, else, `step_3 = -486662 * step_1`
+    `step_2 = step_1 + 1`
 
-    In summary, if `sign`, `v` must not `486661`, and if `!sign`, `v` must not be the
-    multiplicative inverse of `486661`. Since `v` is the output of a hash function, this should
-    have negligible probability. Additionally, since the definition of `sign` is dependent on `v`,
-    it may be truly impossible to reach.
+    We need `step_3 + step_2 \ne 0`, which would require `step_3 \cong -step_2 \mod 2^{255}-19`.
+    This requires:
+    - If `sign`, `step_1 \mod 2^{255}-19 \ne 486661`.
+    - If `!sign`, `(step_1 + 1) \mod 2^{255}-19 \ne (step_1 * 486662) \mod 2^{255}-19` which is
+      equivalent to `(step_1 * 486661) \mod 2^{255}-19 \ne 1`.
+
+    In summary, if `sign`, `step_1` must not equal `486661`, and if `!sign`, `step_1` must not be
+    the multiplicative inverse of `486661`. Since `step_1` is the output of a hash function, this
+    should have negligible probability. Additionally, since the definition of `sign` is dependent
+    on `step_1`, it may be truly impossible to reach.
   */
-  Y *= Z.invert().expect("if sign, v was 486661. if !sign, v was 486661^{-1}");
+  #[allow(non_snake_case)]
+  let Y = (step_3 - step_2) *
+    (step_3 + step_2).invert().expect(&format!(
+      "step_1 ({:?}) was 486661 or 486661^{{-1}} depending on was_square ({}). input: {:?}",
+      step_1,
+      bool::from(derivative_was_square),
+      bytes
+    ));
+
   let mut bytes = Y.to_repr();
-  bytes[31] |= sign.unwrap_u8() << 7;
+  bytes[31] |= (!derivative_was_square).unwrap_u8() << 7;
+
+  /*
+    Ed25519 point decompression works as follows.
+
+    d = (-121665) / 121666
+    x^2 = (y^2 - 1) / ((d y^2) + 1)
+
+    Note `(d y^2) + 1` will always be non-zero as `((2^{255} - 19) - 1) / d` doesn't have a square
+    root modulo `2^{255} - 19`.
+
+    ```sage
+    p = 2**255 - 19
+    d = (-121665) * inverse_mod(121666, p)
+    Mod((p - 1) * inverse_mod(d, p), p).is_square() == False
+    ```
+  */
 
   decompress_point(bytes).expect("point from hash-to-curve wasn't on-curve").mul_by_cofactor()
 }
